1e8b4c553c66d30a9fe905d1f4daf2ce34800744
wlk <william.koelling@gmail.com>
2022-11-23 12:23:02 -0600

    setup chakra

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import { AppProvider } from "./utils/AppContext";
+import { ChakraProvider } from '@chakra-ui/react'
 import Profile from "./pages/Profile"
 import Home from "./pages/Home"
 import './App.css';
 
 function App() {
-
-
   return (
-    <AppProvider>
-      <Home />
-    </AppProvider>
+    <ChakraProvider>
+      <AppProvider>
+        <Home />
+      </AppProvider>
+    </ChakraProvider>
   );
 }
 
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,21 @@
 import { useAppContext } from "../utils/AppContext";
 
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
 function Home() {
     const appCtx = useAppContext();
     const state = appCtx.appState
 
     return (
         // Grid Component
-        <> 
+        <>
+
+            <Card>
+                <CardBody>
+                    <p>Welcome back, {state.userData.name}.</p>
+                </CardBody>
+            </Card>
+
             {state.userData.isManager && (
                 <p> [show manager stuff] </p>
             )}
