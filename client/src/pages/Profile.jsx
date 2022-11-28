ca2a5f0a0545480caedce3f2a4fe017a64fd0bca
Tristan Hanson <thanson1998@live.com>
2022-11-28 10:56:15 -0600

    started work on user profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,15 @@
 import { useAppContext } from "../utils/AppContext";
+import { 
+    Card,
+    CardHeader, 
+    CardBody, 
+    CardFooter, 
+    Heading, 
+    Stack, 
+    StackDivider, 
+    Box, 
+    Text 
+} from '@chakra-ui/react'
 
 function Profile() {
     const appCtx = useAppContext();
@@ -6,7 +17,40 @@ function Profile() {
 
     return (
         <>
-            <p>{state.userData.name}</p>
+            <Card>
+                <CardHeader>
+                    <Heading size='md'>{state.userData.name}</Heading>
+                </CardHeader>
+
+                <CardBody>
+                    <Stack divider={<StackDivider />} spacing='10'>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                        Summary
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                        View a summary of all your clients over the last month.
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                        Overview
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                        Check out the overview of your clients.
+                        </Text>
+                    </Box>
+                    <Box>
+                        <Heading size='xs' textTransform='uppercase'>
+                        Analysis
+                        </Heading>
+                        <Text pt='2' fontSize='sm'>
+                        See a detailed analysis of all your business clients.
+                        </Text>
+                    </Box>
+                    </Stack>
+                </CardBody>
+            </Card>
         </>
     )
 }
