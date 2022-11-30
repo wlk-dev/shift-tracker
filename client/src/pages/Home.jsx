7a41b7f33ff134cba3cdb86efcca785172043b20
wlk <william.koelling@gmail.com>
2022-11-30 12:52:18 -0600

    data now persists if nothing new is chosen

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,7 @@
-import { useAppContext } from "../utils/AppContext";
-
-import { Card, CardHeader, Heading, Box } from '@chakra-ui/react'
-
 import TileMenu from "../components/TileMenu";
 import Header from "../components/Header";
 
 function Home() {
-    const appCtx = useAppContext();
-    const state = appCtx.appState
 
     return (
         // Grid Component
